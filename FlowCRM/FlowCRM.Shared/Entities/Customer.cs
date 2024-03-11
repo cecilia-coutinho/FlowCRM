c5d55840faa5f37d9f1e09d754cd8a48bc5389cd
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-03-11 18:19:40 +0100

    ClassLibrary Added

---
+++ FlowCRM/FlowCRM.Shared/Entities/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowCRM.Shared.Entities
+{
+    public class Customer
+    {
+        public Guid CustomerId { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public string? Phone { get; set; }
+
+    }
+}
