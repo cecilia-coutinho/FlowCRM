08fe80ca1bde6f99fae036ad4200da679fa3603d
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-04-29 16:18:13 +0200

    Refactor: New property added

--- FlowCRM/FlowCRM.Shared/Entities/Deal.cs
+++ FlowCRM/FlowCRM.Shared/Entities/Deal.cs
@@ -10,6 +10,7 @@ namespace FlowCRM.Shared.Entities
 		public class Deal
 		{
 				public Guid DealId { get; set; }
+				public string? DealName { get; set; }
 				public Guid? FkCustomerId { get; set; }
 				public Guid? FkCompanyId { get; set; }
 				public required decimal DealAmount { get; set; }
@@ -17,11 +18,11 @@ namespace FlowCRM.Shared.Entities
 				public required Guid FkPriorityId { get; set; }
 				public DateTime? CreatedAt { get; set; }
 				public DateTime? UpdatedAt { get; set; }
-        public string? CreatedBy { get; set; }
-        public string? UpdatedBy { get; set; }
+				public string? CreatedBy { get; set; }
+				public string? UpdatedBy { get; set; }
 
-        // Navigation properties
-        public ICollection<Activity>? Activities { get; set; }
+				// Navigation properties
+				public ICollection<Activity>? Activities { get; set; }
 				public virtual Customer? Customers { get; set; }
 				public virtual Company? Companies { get; set; }
 				public virtual DealStatus? DealsStatus { get; set; }
