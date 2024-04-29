909c819546b219502a51f248f585b2e7a9495e6b
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-04-29 16:23:55 +0200

    Update: Added implementation for new IRepositories

---
+++ FlowCRM/FlowCRM/Implementations/ActivityTypeRepository.cs
@@ -0,0 +1,34 @@
+﻿using FlowCRM.Data;
+using FlowCRM.Shared.Entities;
+using FlowCRM.Shared.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowCRM.Implementations
+{
+		public class ActivityTypeRepository : IActivityTypeRepository
+		{
+				private readonly ApplicationDbContext _context;
+
+				public ActivityTypeRepository(ApplicationDbContext context)
+				{
+						_context = context;
+				}
+				public async Task<IEnumerable<ActivityType>> GetActivitiesTypeAsync()
+				{
+						return await _context.ActivitiesType.ToListAsync();
+				}
+
+				public async Task<ActivityType> GetActivityTypeAsync(Guid id)
+				{
+						var activityType = await _context.ActivitiesType.FindAsync(id);
+
+						if (activityType == null)
+						{
+								throw new Exception($"No activity type found with ID {id}");
+						}
+
+						return activityType;
+				}
+
+		}
+}
---
+++ FlowCRM/FlowCRM/Implementations/PriorityRepository.cs
@@ -0,0 +1,34 @@
+﻿using FlowCRM.Data;
+using FlowCRM.Shared.Entities;
+using FlowCRM.Shared.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowCRM.Implementations
+{
+    public class PriorityRepository : IPriorityRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PriorityRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Priority>> GetPrioritiesAsync()
+        {
+            return await _context.Priorities.ToListAsync();
+        }
+
+        public async Task<Priority> GetPriorityAsync(Guid id)
+        {
+            var priority = await _context.Priorities.FindAsync(id);
+
+            if (priority == null)
+            {
+                throw new Exception($"No priority found with ID {id}");
+            }
+
+            return priority;
+        }
+    }
+}
