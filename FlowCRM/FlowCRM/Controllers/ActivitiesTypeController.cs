0b338b1043792eb835141826019e5509012db49e
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-04-29 16:17:43 +0200

    Update: New Controllers Added

---
+++ FlowCRM/FlowCRM/Controllers/ActivitiesTypeController.cs
@@ -0,0 +1,34 @@
+﻿using FlowCRM.Shared.Entities;
+using FlowCRM.Shared.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowCRM.Controllers
+{
+		[Route("api/[controller]")]
+		[ApiController]
+		public class ActivitiesTypeController : ControllerBase
+		{
+				private readonly IActivityTypeRepository _activityTypeRepository;
+
+				public ActivitiesTypeController(IActivityTypeRepository activityTypeRepository)
+				{
+						_activityTypeRepository = activityTypeRepository;
+				}
+
+				// GET: api/ActivitiesType/All-ActivitiesType
+				[HttpGet("All-ActivitiesType")]
+				public async Task<ActionResult<IEnumerable<ActivityType>>> GetActivitiesTypeAsync()
+				{
+						var activitiesType = await _activityTypeRepository.GetActivitiesTypeAsync();
+						return Ok(activitiesType);
+				}
+
+				// GET: api/ActivitiesType/Single-ActivityType/5
+				[HttpGet("Single-ActivityType/{id}")]
+				public async Task<ActionResult<ActivityType>> GetActivityTypeAsync(Guid id)
+				{
+						var activityType = await _activityTypeRepository.GetActivityTypeAsync(id);
+						return Ok(activityType);
+				}
+		}
+}
---
+++ FlowCRM/FlowCRM/Controllers/PrioritiesController.cs
@@ -0,0 +1,34 @@
+﻿using FlowCRM.Shared.Entities;
+using FlowCRM.Shared.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlowCRM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrioritiesController : ControllerBase
+    {
+        private readonly IPriorityRepository _priorityRepository;
+
+        public PrioritiesController(IPriorityRepository priorityRepository)
+        {
+            _priorityRepository = priorityRepository;
+        }
+
+        // GET: api/Priorities/All-Priorities
+        [HttpGet("All-Priorities")]
+        public async Task<ActionResult<IEnumerable<Priority>>> GetPriorities()
+        {
+            var priorities = await _priorityRepository.GetPrioritiesAsync();
+            return Ok(priorities);
+        }
+
+        // GET: api/Priorities/Single-Priority/5
+        [HttpGet("Single-Priority/{id}")]
+        public async Task<ActionResult<Priority>> GetPriority(Guid id)
+        {
+            var priority = await _priorityRepository.GetPriorityAsync(id);
+            return Ok(priority);
+        }
+    }
+}
