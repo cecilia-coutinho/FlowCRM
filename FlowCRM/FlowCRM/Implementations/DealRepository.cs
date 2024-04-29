f2cb6507b9c9d8204f75419804e615a6610b8ea8
cecilia.coutinho <cecilia.coutinho@outlook.com>
2024-04-29 12:15:09 +0200

    repository implementation

---
+++ FlowCRM/FlowCRM/Implementations/DealRepository.cs
@@ -0,0 +1,75 @@
+﻿using FlowCRM.Data;
+using FlowCRM.Shared.Entities;
+using FlowCRM.Shared.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowCRM.Implementations
+{
+	public class DealRepository : IDealRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public DealRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<IEnumerable<Deal>> GetDealsAsync()
+		{
+			return await _context.Deals.ToListAsync();
+		}
+
+		public async Task<Deal> GetDealAsync(Guid id)
+		{
+			var deal = await _context.Deals.FindAsync(id);
+
+			if (deal == null)
+			{
+				throw new Exception($"No deal found with ID {id}");
+			}
+
+			return deal;
+		}
+
+		public async Task<Deal> AddDealAsync(Deal deal)
+		{
+			if (deal == null)
+			{
+				throw new Exception($"No deal found: {deal}");
+			}
+
+			deal.CreatedAt = DateTime.Now;
+			_context.Deals.Add(deal);
+			await _context.SaveChangesAsync();
+			return deal;
+		}
+		public async Task<Deal> UpdateDealAsync(Deal deal)
+		{
+			var existingDeal = await _context.Deals.FindAsync(deal.DealId);
+
+			if (existingDeal == null)
+			{
+				throw new Exception($"No deal found: {deal}");
+			}
+
+			existingDeal = deal;
+			existingDeal.UpdatedAt = DateTime.Now;
+			_context.Entry(existingDeal).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+			return existingDeal;
+		}
+
+		public async Task<Deal> DeleteDealAsync(Guid id)
+		{
+			var deal = await _context.Deals.FindAsync(id);
+
+			if (deal == null)
+			{
+				throw new Exception($"No deal found with ID {id}");
+			}
+
+			_context.Deals.Remove(deal);
+			await _context.SaveChangesAsync();
+			return deal;
+		}
+	}
+}
---
+++ FlowCRM/FlowCRM/Implementations/LeadRepository.cs
@@ -0,0 +1,75 @@
+﻿using FlowCRM.Data;
+using FlowCRM.Shared.Entities;
+using FlowCRM.Shared.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowCRM.Implementations
+{
+	public class LeadRepository : ILeadRepository
+	{
+
+		private readonly ApplicationDbContext _context;
+
+		public LeadRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public async Task<IEnumerable<Lead>> GetLeadsAsync()
+		{
+			return await _context.Leads.ToListAsync();
+		}
+
+		public async Task<Lead> GetLeadAsync(Guid id)
+		{
+			var lead = await _context.Leads.FindAsync(id);
+
+			if (lead == null)
+			{
+				throw new Exception($"No lead found with ID {id}");
+			}
+			return lead;
+		}
+
+		public async Task<Lead> AddLeadAsync(Lead lead)
+		{
+			if (lead == null)
+			{
+				throw new Exception($"No lead found: {lead}");
+			}
+
+			lead.CreatedAt = DateTime.Now;
+			_context.Leads.Add(lead);
+			await _context.SaveChangesAsync();
+			return lead;
+		}
+		public async Task<Lead> UpdateLeadAsync(Lead lead)
+		{
+			var existingLead = await _context.Leads.FindAsync(lead.LeadId);
+
+			if (existingLead == null)
+			{
+				throw new Exception($"No lead found: {lead}");
+			}
+
+			existingLead = lead;
+			existingLead.UpdatedAt = DateTime.Now;
+			_context.Entry(existingLead).State = EntityState.Modified;
+			await _context.SaveChangesAsync();
+			return existingLead;
+		}
+
+		public async Task<Lead> DeleteLeadAsync(Guid id)
+		{
+			var lead = await _context.Leads.FindAsync(id);
+
+			if (lead == null)
+			{
+				throw new Exception($"No lead found with ID {id}");
+			}
+
+			_context.Leads.Remove(lead);
+			await _context.SaveChangesAsync();
+			return lead;
+		}
+	}
+}
